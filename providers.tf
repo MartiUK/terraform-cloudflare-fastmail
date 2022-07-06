746c80b41bf009e1cda8ac3dc0d2273916c6deaf
Martin Kemp <me@martinke.mp>
2022-07-06 15:17:09 +0100

    simplify version requirements
    Signed-off-by: Martin Kemp <me@martinke.mp>

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.0.0"
+  required_version = "~> 1"
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 3.0"
+      version = "~> 3"
     }
   }
 }
